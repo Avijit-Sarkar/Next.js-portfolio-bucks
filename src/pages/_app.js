c49cb79a30c2e0bc0745b75e946748ec21a82205
Avijit <avijitsarkar2222@gmail.com>
2023-05-01 01:11:24 +0530

    transition done.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,11 @@ import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
 // import { ThemeProvider } from "next-themes";
 import "@/styles/globals.css";
+import { AnimatePresence } from "framer-motion";
 
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -12,6 +14,7 @@ const montserrat = Montserrat({
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -23,7 +26,9 @@ export default function App({ Component, pageProps }) {
         className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
       >
         <NavBar />
-        <Component {...pageProps} />
+        <AnimatePresence mode="wait">
+          <Component key={router.asPath} {...pageProps} />
+        </AnimatePresence>
         <Footer />
       </main>
       {/* </ThemeProvider> */}
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -10,6 +10,7 @@ import { motion, useMotionValue } from "framer-motion";
 import article3 from "../../public/images/articles/create modal component in react using react portals.png";
 import article4 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
 import article5 from "../../public/images/articles/pagination component in reactjs.jpg";
+import TransitionEffect from "@/components/TransitionEffect";
 
 const FramerImage = motion(Image);
 
@@ -112,6 +113,7 @@ const articles = () => {
         <title>Bucks | Articles Page</title>
         <meta name="description" content="any description" />
       </Head>
+      <TransitionEffect />
       <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light">
         <Layout className="pt-16">
           <AnimatedText
