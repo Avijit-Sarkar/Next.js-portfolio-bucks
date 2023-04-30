871aceff4b460300d9a6a5560471b120f37b5360
Avijit <avijitsarkar2222@gmail.com>
2023-04-30 22:07:34 +0530

    making the home page responsive

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 const quote = {
   initial: {
@@ -28,18 +28,18 @@ const singleWord = {
   },
 };
 
-const AnimatedText = ({ text, className = '' }) => {
+const AnimatedText = ({ text, className = "" }) => {
   return (
-    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
+    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden sm:py-0">
       <motion.h1
         className={`inline-block w-full text-dark dark:text-light font-bold capitalize text-8xl ${className}`}
         variants={quote}
         initial="initial"
         animate="animate"
       >
-        {text.split(' ').map((word, index) => (
+        {text.split(" ").map((word, index) => (
           <motion.span
-            key={word + '-' + index}
+            key={word + "-" + index}
             className="inline-block"
             variants={singleWord}
           >
