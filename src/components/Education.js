f46716b32bdd72db6fefcaa3c569884193db7ced
Avijit <avijitsarkar2222@gmail.com>
2023-04-30 13:35:46 +0530

    dark mode in education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
-import { motion, useScroll } from 'framer-motion';
-import LiIcon from './LiIcon';
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
+import LiIcon from "./LiIcon";
 
 const Details = ({ type, time, place, info }) => {
   const ref = useRef(null);
@@ -13,10 +13,10 @@ const Details = ({ type, time, place, info }) => {
       <motion.div
         initial={{ y: 50 }}
         whileInView={{ y: 0 }}
-        transition={{ duration: 0.5, type: 'spring' }}
+        transition={{ duration: 0.5, type: "spring" }}
       >
         <h3 className="capitalize font-bold text-2xl">{type}</h3>
-        <span className="capitalize font-medium text-dark/75">
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
           {time} | {place}
         </span>
         <p className="font-medium w-full">{info}</p>
@@ -29,7 +29,7 @@ const Education = () => {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ['start end', 'center start'],
+    offset: ["start end", "center start"],
   });
   return (
     <div className="my-64">
@@ -38,7 +38,7 @@ const Education = () => {
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"
         />
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
