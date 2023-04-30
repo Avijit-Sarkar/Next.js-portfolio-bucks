f9edffa5c3300de593607c61ea3e2ab5cc381ef1
Avijit <avijitsarkar2222@gmail.com>
2023-04-30 22:19:22 +0530

    making responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
                 text="Turning Vision Into Reality With Code And Design."
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
-              <p className="my-4 text-base font-medium">
+              <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
@@ -44,7 +44,7 @@ export default function Home() {
                 <Link
                   href="/dummy.pdf"
                   target={"_blank"}
-                  className="flex items-center bg-dark text-light dark:bg-light dark:text-dark p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:dark:bg-dark hover:text-dark hover:dark:text-light border border-solid border-transparent hover:border-dark hover:dark:border-light"
+                  className="flex items-center bg-dark text-light dark:bg-light dark:text-dark p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:dark:bg-dark hover:text-dark hover:dark:text-light border border-solid border-transparent hover:border-dark hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}
                 >
                   Resume
@@ -53,7 +53,7 @@ export default function Home() {
                 <Link
                   href="mailto:example@example.com"
                   target={"_blank"}
-                  className="ml-4 text-lg font-medium capitalize text-dark dark:text-light underline"
+                  className="ml-4 text-lg font-medium capitalize text-dark dark:text-light underline md:text-base"
                 >
                   Contact
                 </Link>
