934aaab0d80b5b81f4bb0fabb5f09cf99b25a57b
Avijit <avijitsarkar2222@gmail.com>
2023-04-30 14:08:58 +0530

    dark mode completed

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,15 +1,15 @@
-import AnimatedText from '@/components/AnimatedText';
-import Layout from '@/components/layout';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useRef } from 'react';
-import article1 from '../../public/images/articles/pagination component in reactjs.jpg';
-import article2 from '../../public/images/articles/create loading screen in react js.jpg';
-import { motion, useMotionValue } from 'framer-motion';
-import article3 from '../../public/images/articles/create modal component in react using react portals.png';
-import article4 from '../../public/images/articles/form validation in reactjs using custom react hook.png';
-import article5 from '../../public/images/articles/pagination component in reactjs.jpg';
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useRef } from "react";
+import article1 from "../../public/images/articles/pagination component in reactjs.jpg";
+import article2 from "../../public/images/articles/create loading screen in react js.jpg";
+import { motion, useMotionValue } from "framer-motion";
+import article3 from "../../public/images/articles/create modal component in react using react portals.png";
+import article4 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
+import article5 from "../../public/images/articles/pagination component in reactjs.jpg";
 
 const FramerImage = motion(Image);
 
@@ -19,13 +19,13 @@ const MovingImg = ({ title, img, link }) => {
   const imgRef = useRef(null);
 
   function handleMouse(event) {
-    imgRef.current.style.display = 'inline-block';
+    imgRef.current.style.display = "inline-block";
     x.set(event.pageX);
     y.set(-10);
   }
 
   function handleMouseLeave(event) {
-    imgRef.current.style.display = 'none';
+    imgRef.current.style.display = "none";
     x.set(0);
     y.set(0);
   }
@@ -56,20 +56,24 @@ const MovingImg = ({ title, img, link }) => {
 const Article = ({ img, title, date, link }) => {
   return (
     <motion.li
-      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4"
+      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light
+      dark:bg-dark dark:text-light
+      "
       initial={{ y: 200 }}
-      whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
+      whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
       viewport={{ once: true }}
     >
       <MovingImg title={title} img={img} link={link} />
-      <span className="text-primary font-semibold pl-4">{date}</span>
+      <span className="text-primary dark:text-primaryDark font-semibold pl-4">
+        {date}
+      </span>
     </motion.li>
   );
 };
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
+    <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl dark:bg-dark dark:border-light">
       <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
       <Link
         href={link}
@@ -90,7 +94,9 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
         </h2>
       </Link>
       <p className="text-sm mb-2">{summary}</p>
-      <span className="text-primary font-semibold">{time}</span>
+      <span className="text-primary dark:text-primaryDark font-semibold">
+        {time}
+      </span>
     </li>
   );
 };
@@ -102,7 +108,7 @@ const articles = () => {
         <title>Bucks | Articles Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
+      <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light">
         <Layout className="pt-16">
           <AnimatedText text="Words Can Change The World!" className="mb-16" />
           <ul className="grid grid-cols-2 gap-16">
